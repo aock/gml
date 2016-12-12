361a1153307e10c501cee79a624e3bbf02fae35b
Jonas Rothe <jorothe@uos.de>
2016-12-12 19:43:45 +0100

    merge

--- CombinedCyclePowerPlant/ccpp_polynomial.py
+++ CombinedCyclePowerPlant/ccpp_polynomial.py
@@ -9,45 +9,19 @@ Template for the CCPP-Challenge
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
+flag = True
 
-"""
-Calcuate the MSE on a given dataset
-@param h The hypothesis to evaluate
-@param x The input values of the test data
-@param y The output value of the test data
-@return The mean squared error
-"""
-def calculateError(h, x, y):
-    error = 0
-    for el in zip(x, y):
-        yh = h.evaluate(el[0])
-        y = el[1]
-        error += (yh - y)**2
-    error /= len(x)
-    return error
-
-
-def normalize(x):
-    for i in range(len(x[0])):
-        x[:,i] -= np.amin(x[:,i])
-        x[:,i] /= np.amax(x[:,i])
-    return x
-
-
-"""
-This class contains a linear regressor on a polynomial-approximator
-for 1D problems
-"""
 class Polynomial():
     """
     Constructor
     @param deg The degree of the polynomial
     """
 
-    def __init__(self, deg):
+    def __init__(self, deg, ):
         self.deg = deg
-        self.w = np.zeros(deg + 1)
+        self.w = None
 
     """
     Tansformation function (phi)
@@ -56,9 +30,16 @@ class Polynomial():
     """
 
     def transform(self, x):
-        func = [x_dim ** i for x_dim in x for i in range(self.deg + 1) ]
+        global flag
+        func = [1]
+        gen = [x_dim ** i for x_dim in x for i in range(1, self.deg + 1)]
+        for g in gen:
+            func.append(g)
         for x_ in x:
             func.append(np.sin(x_))
+        if flag:
+            print(func)
+            flag = False
         return func
 
     """
@@ -80,26 +61,3 @@ class Polynomial():
     def evaluate(self, x):
         Xdagger = self.transform(x)
         return np.dot(self.w, Xdagger), Xdagger
-
-    
-if __name__ == "__main__":
-    # Read data from file
-    data = np.genfromtxt("ccpp_first_batch.txt", delimiter=",")
-    # Shuffle data
-    np.random.shuffle(data)
-    # Seperate data in input and output values
-    dataX = [el[0:4] for el in data]
-    dataY = [el[4] for el in data]
-    # Normalize input data
-    dataX = normalize(np.asarray(dataX))
-    # Split data in training and test set
-    trainX = dataX[:6000]
-    trainY = dataY[:6000]
-    testX = dataX[6000:]
-    testY = dataY[6000:]
-    # Create hypothesis set
-    h = Polynomial(10)
-    # Learn on the training data
-    h.learn(trainX, trainY)
-    # Display the in-sample-error
-    print('In-Sample-Error:', calculateError(h, testX, testY))
--- CombinedCyclePowerPlant/evaluate.py
+++ CombinedCyclePowerPlant/evaluate.py
@@ -4,7 +4,6 @@ from ccpp_RLS import RLS
 from ccpp_linear import GLT
 from ccpp_polynomial import Polynomial
 from matplotlib.pyplot import plot as plt
-
 """
 Calcuate the MSE on a given dataset
 @param h The hypothesis to evaluate
@@ -40,20 +39,28 @@ if __name__ == "__main__":
     data = np.genfromtxt("ccpp_first_batch.txt", delimiter=",")
 
     #np.random.shuffle(data)
+    dataX = []
+    dataY = []
+
+    random_indices = np.arange(0,7000)
+    np.random.shuffle(random_indices)
+
+    indices_check = list(random_indices[:2000])
+    indices_train = list(random_indices[2000:])
 
     # Seperate data in input and output values
-    dataX = [el[0:4] for el in data]
-    dataY = [el[4] for el in data]
+    dataX = np.array([el[0:4] for el in data])
+    dataY = np.array([el[4] for el in data])
 
     # Normalize input data
     dataX = normalize(np.asarray(dataX))
 
     saveValues = 5000
 
-    dataXTrain  = dataX[:saveValues]
-    dataYTrain  = dataY[:saveValues]
-    dataXTest   = dataX[saveValues:]
-    dataYTest   = dataY[saveValues:]
+    dataXTrain  = dataX[indices_train]
+    dataYTrain  = dataY[indices_train]
+    dataXTest   = dataX[indices_check]
+    dataYTest   = dataY[indices_check]
 
     print("Polynome")
     for i in range(4, 16, 1):
@@ -66,15 +73,17 @@ if __name__ == "__main__":
     print("--------------------------------")
     print("GLT")
     # Create hypothesis set
-    for i in range(18, 22, 1):
-        h = GLT(i, 4)
-        # Learn on the training data
-        h.learn(dataXTrain, dataYTrain)
-        # Display the in-sample-error
-        print('In-|Out-Sample-Error:  %d - %f | %f' %
+    h = GLT(20, 4)
+    # Learn on the training data
+    h.learn(dataXTrain, dataYTrain)
+    # Display the in-sample-error
+    print('In-|Out-Sample-Error:  %d - %f | %f' %
         (i, calculateError(h, dataXTrain, dataYTrain), calculateError(h, dataXTest, dataYTest)))
-    
+
+
+
     print("--------------------------------")
+    print("RLS")
     for i in range(18, 22, 1):
         h = GLT(i, 4)
         l = RLS(i * 4, 1)
