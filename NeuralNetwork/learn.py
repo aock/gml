26b85f40ef584d44841a8f387bc55b544da94522
Jonas Rothe <jorothe@uos.de>
2017-01-25 07:57:59 +0100

    save

--- NeuralNetwork/learn.py
+++ NeuralNetwork/learn.py
@@ -33,24 +33,25 @@ if __name__ == "__main__":
 
     k = eval(sys.argv[1])
     targetFunction = eval(sys.argv[2])
-    saveName = "save_" + sys.argv[1] + "_" + sys.argv[2]
+    saveName = "save_" + sys.argv[1] + "_" + sys.argv[2] + "_noise_0.1"
 
     print k, targetFunction, saveName
-    error = np.zeros([200, 200])
+    error = np.zeros([200])
 
     print("Starting")
-    for i in range(1,201):
+    for i in range(1, 201, 1):
         t = time.time()
-        for j, r in enumerate(frange(0, 1, 0.05)):
-            X, Y = targetFunction(n=i, noise=r)
-            if len(X) == 1:
-                X = X.reshape(1, -1)
-            else:
-                X = X.reshape(-1, 1)
-            a = MLPRegressor(solver='sgd', activation ='tanh',
-                       hidden_layer_sizes=[5]*k, max_iter=int(1e6), tol=1e-8)
-            m = a.fit(X, Y)
-            error[i-1, j] = calculategtLoss(targetFunction, m)
+        #for j, r in enumerate(frange(0, 1, 0.05)):
+        r = 0.1
+        X, Y = targetFunction(n=i, noise=r)
+        if len(X) == 1:
+            X = X.reshape(1, -1)
+        else:
+            X = X.reshape(-1, 1)
+        a = MLPRegressor(solver='sgd', activation ='tanh',
+                   hidden_layer_sizes=[5]*k, max_iter=int(1e6), tol=1e-8)
+        m = a.fit(X, Y)
+        error[i-1] = calculategtLoss(targetFunction, m)
         print(i, "finished in", time.time() - t, "seconds | error: ")
 
     with open(saveName, "wb") as f:
